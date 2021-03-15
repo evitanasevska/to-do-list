d1bbffc1f04ddc52f79425dc53754a78bf1417c5
Evita <nasevska.evita@gmail.com>
2021-03-15 02:07:13 +0100

    Button fix

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const express = require("express");
 const app = express();
 
 //heroku
-let port = process.env.PORT
-if(port == null || port == ""){
-  port = 3000
-}
-app.listen(port, function () {
-  console.log("Server is up and running on port 3000.");
-});
+app.listen(process.env.PORT || 4000, function(){
+  console.log("Server is running on port 3000.");
+})
+
+// app.listen(4000, function(){
+//   console.log("Server running on port 3000.")
+// })
 
 const _ = require("lodash")
 
