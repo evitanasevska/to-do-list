e079625e0144c1e9ff21816a2adce6a003426b81
Evita <nasevska.evita@gmail.com>
2021-03-15 01:10:18 +0100

    Heroku setup

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 //start server
 const express = require("express");
 const app = express();
-app.listen(3000, function () {
+
+//heroku
+let port = process.env.PORT
+if(port == null || port == ""){
+  port = 3000
+}
+app.listen(port, function () {
   console.log("Server is up and running on port 3000.");
 });
 
